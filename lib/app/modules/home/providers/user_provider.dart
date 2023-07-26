dad7086f606c12017f1d6d0e27d7f7846c289d87
Saad0149 <k190149@nu.edu.pk>
2023-07-26 14:07:22 +0500

    StateMixin to generate a List using a REST API

--- lib/app/modules/home/providers/user_provider.dart
+++ lib/app/modules/home/providers/user_provider.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 
 class UserProvider extends GetConnect {
   Future<List<dynamic>> getUser() async {
-    final response = await get('https://randomuser.me/api/?results=20');
+    final response = await get('https://randomuser.me/api/?results=50');
     if (response.status.hasError) {
       return Future.error(response.statusText as Object);
     } else {
